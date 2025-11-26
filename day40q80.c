f8920716e050eeb8d536198d9c9b4742bdbf8df2
siddhantraj16 <siddhant.29472@stu.upes.ac.in>
2025-11-26 21:33:58 +0530

    Add files via upload

---
+++ day40q80.c
@@ -0,0 +1,78 @@
+//Q80: Multiply two matrices.
+
+/*
+Sample Test Cases:
+Input 1:
+2 3
+1 2 3
+4 5 6
+3 2
+7 8
+9 10
+11 12
+Output 1:
+58 64
+139 154
+
+*/
+#include <stdio.h>
+
+int main() {
+    int r1, c1, r2, c2;
+
+    // Read dimensions of first matrix
+    scanf("%d %d", &r1, &c1);
+    int A[r1][c1];
+    
+    // Read first matrix
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c1; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    // Read dimensions of second matrix
+    scanf("%d %d", &r2, &c2);
+    int B[r2][c2];
+    
+    // Read second matrix
+    for (int i = 0; i < r2; i++) {
+        for (int j = 0; j < c2; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    // Check if multiplication is possible
+    if (c1 != r2) {
+        printf("Matrix multiplication not possible\n");
+        return 0;
+    }
+
+    int result[r1][c2];
+
+    // Initialize result matrix
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            result[i][j] = 0;
+        }
+    }
+
+    // Perform multiplication
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            for (int k = 0; k < c1; k++) {
+                result[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    // Print result matrix
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
